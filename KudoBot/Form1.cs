f636caefc00d4738fd9a86ae77227a4ef886e491
mrgucci1 <dalquist.keaton221@gmail.com>
2021-04-16 19:45:00 -0500

    Adding the ability to auto comment on posts when certain people post an activity

--- KudoBot/Form1.cs
+++ KudoBot/Form1.cs
@@ -24,44 +24,49 @@ namespace KudoBot
         private void Form1_Load(object sender, EventArgs e)
         {
             textBox1.Text = "0";
-            textBox2.Text = "1000";
+            textBox2.Text = "200";
+            button1_Click(sender, e);
         }
         private async void button1_Click(object sender, EventArgs e)
         {
+            await PutTaskDelay(5);
             //Navigate to strava
             webView.CoreWebView2.Navigate("https://www.strava.com/dashboard");
             await PutTaskDelay(3);
+            //Click on login button
+            await evaluateScript("document.getElementsByClassName('btn btn-block google-button')[0].click();");
+            await PutTaskDelay(5);
             progressBar1.Maximum = (Convert.ToInt32(textBox2.Text)) * 10;
+            string[] comments = { "just chilly", "mega chilly", "DALQ STRENGTH", "COOOOOOOOOOOKIE", "good job :D" };
+            var rand = new Random();
             //Give Kudos up until number that the user specified
-            for (int i = 0;i<(Convert.ToInt32(textBox2.Text));i++)
+            for (int i = 0; i < (Convert.ToInt32(textBox2.Text)); i++)
             {
                 //Focus on Kudo Button
                 await evaluateScript($@"document.getElementsByClassName('btn btn-icon btn-icon-only btn-kudo btn-xs js-add-kudo')[0].focus();");
                 //Click Kudo Button
                 await evaluateScript($@"document.getElementsByClassName('btn btn-icon btn-icon-only btn-kudo btn-xs js-add-kudo')[0].click();");
+                //If a kudo dialog shows up, click the x
+                await evaluateScript("document.getElementsByClassName('btn btn-unstyled btn-close')[0].click();");
+                //Loop Through activities and find ones with names we want to comment on
+                /*for (int j = 0; j < Convert.ToInt32(textBox2.Text); j++)
+                {
+                    var returnVar = await webView.ExecuteScriptAsync($"document.getElementsByClassName('entry-owner')[{j}].innerText.toString()");
+                    if(returnVar.ToString().Contains("Austen Dalquist") || returnVar.ToString().Contains("Kirsten Dalquist"))
+                    {
+                        //Click on comment button
+                        await evaluateScript($"document.getElementsByClassName('btn btn-comment btn-icon btn-icon-only btn-xs empty')[{j}].click();");
+                        await evaluateScript($"document.getElementsByClassName('form-control')[{7+j}].innerText = '{comments[rand.Next(0, 5)]}'");
+                        //await evaluateScript($"document.getElementsByClassName('text-footnote btn btn-default btn-sm')[{j}].click();");
+                    }
+                }*/
                 await PutTaskDelay(1);
                 //Update Kudo Given textbox
                 textBox1.Text = (Convert.ToInt32(textBox1.Text.ToString()) + 1).ToString();
-                //Every 10 Kudos, press space to go to botton of page
-                if(i%10==0)
-                {
-                    //if not first case
-                    if(i!=0)
-                    {
-                        //get webview window in forground to press space
-                        Process p = Process.GetProcessesByName("msedgewebview2").FirstOrDefault();
-                        if (p != null)
-                        {
-                            IntPtr h = p.MainWindowHandle;
-                            SetForegroundWindow(h);
-                            SendKeys.SendWait(" ");
-                            await PutTaskDelay(1);
-                            SendKeys.SendWait(" ");
-                        }
-                    }     
-                }
                 progressBar1.PerformStep();
-            }  
+            }
+            await PutTaskDelay(3);
+            Environment.Exit(0);
         }
         public async Task evaluateScript(string functionString)
         {
